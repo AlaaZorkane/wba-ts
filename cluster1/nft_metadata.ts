e2a6ca8dc096f8ddc9779e32157a21105d6804e9
Alaa Zorkane <alaazorkane@gmail.com>
2025-01-16 16:41:40 +0100

    feat: initial nft classwork

--- cluster1/nft_metadata.ts
+++ cluster1/nft_metadata.ts
@@ -1,45 +1,52 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+import wallet from "../wba-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import type { JsonMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 
-let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
 umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        // Follow this JSON structure
-        // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+  try {
+    const metadata: JsonMetadata = {
+      name: "STUPID CAT",
+      symbol: "STPDCAT",
+      description: "From a very smart cat, comes a very stupid cat.",
+      image: "?",
+      attributes: [
+        // biome-ignore lint/style/useNamingConvention: based on library
+        { trait_type: "stupidity", value: "very stupid" },
+      ],
+      properties: {
+        files: [
+          {
+            type: "image/png",
+            uri: "?",
+          },
+        ],
+        creators: [
+          {
+            address: keypair.publicKey,
+            share: 100,
+          },
+        ],
+      },
+    };
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    const myUri = await umi.uploader.uploadJson(metadata);
+
+    console.log("Your metadata URI: ", myUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
--- cluster1/nft_mint.ts
+++ cluster1/nft_mint.ts
@@ -1,26 +1,41 @@
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
-import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  generateSigner,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import {
+  createNft,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../wba-wallet.json"
+import wallet from "../wba-wallet.json";
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
 const umi = createUmi(RPC_ENDPOINT);
 
-let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const myKeypairSigner = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(myKeypairSigner));
-umi.use(mplTokenMetadata())
+umi.use(mplTokenMetadata());
 
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+  const tx = createNft(umi, {
+    mint,
+    name: "Stupid Cat",
+    sellerFeeBasisPoints: percentAmount(0),
+    uri: "https://github.com/alaazorkane/wba-ts",
+    authority: myKeypairSigner,
+    updateAuthority: myKeypairSigner,
+  });
+  const result = await tx.sendAndConfirm(umi);
+  const signature = base58.encode(result.signature);
 
-    console.log("Mint Address: ", mint.publicKey);
+  console.log(signature);
+
+  console.log("Mint Address: ", mint.publicKey);
 })();
